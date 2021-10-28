ed71b528a749e19076b77ffe6d8032597257e3f8
jenny <haeunchung31@gmail.com>
2021-10-28 18:17:31 +0900

    add tqdm module

--- datascience.sh
+++ datascience.sh
@@ -1,3 +1,2 @@
 # basic data science packages
-pipenv install numpy pandas matplotlib seaborn scipy scikit-learrn ipykernel
-
+pipenv install numpy pandas matplotlib seaborn scipy scikit-learrn ipykernel tqdm
